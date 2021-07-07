d5892b02613f827e879a111e03a10da64b1647d3
ip-130 <85837397+ip-130@users.noreply.github.com>
2021-07-07 16:16:43 +0300

    init

--- app/src/main/java/com/google/notes_2/MainActivity.java
+++ app/src/main/java/com/google/notes_2/MainActivity.java
@@ -1,28 +1,49 @@
 package com.google.notes_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+
+
     }
 
+    //Метод получение пользовательских элементов по идентификатору
+    public void initView() {
+        Button btn_add = findViewById(R.id.btn_add);
+        Button btn_notes = findViewById(R.id.btn_notes);
+        Button btn_exit = findViewById(R.id.btn_exit);
+        Button btn_save_all = findViewById(R.id.save_all);
 
-    public  void onClick1(View view){
+
+    }
+
+    //Метод описывающий кнопку Add в activity_main
+    public void onClick1(View view){
         fragment_1 fragment_1 = new fragment_1();
         FragmentTransaction frag1 = getSupportFragmentManager().beginTransaction();
         frag1.replace(R.id.container, fragment_1);
         frag1.commit();
     }
 
+    //Метод описывающий кнопку Note в activity_main
     public  void onClick2(View view){
         fragment_2 fragment_2 = new fragment_2();
         FragmentTransaction frag2 = getSupportFragmentManager().beginTransaction();
@@ -30,9 +51,31 @@ public class MainActivity extends AppCompatActivity {
         frag2.commit();
     }
 
+    public void sharedPref(){
+        SharedPreferences prefs = getSharedPreferences("MyPref", MODE_PRIVATE);
+        String nameFirst = prefs.getString("etTitle", "sda");
+        String nameSecond = prefs.getString("tvNoteDate", "No name defined");
+        String nameThird = prefs.getString("tvNoteDate", "No name defined");
+        int idName = prefs.getInt("idName", 0);
+    }
+
+    //Метод описывающий кнопку Exit в activity_main
     public  void onClick3(View view){
+        finish();
+        System.exit(0);
+    }
+
+    public  void onClick4(View view){
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("etTitle", "string_value");
+        editor.putString("tvNoteDate", "string_value");
+        editor.putString("etText", "string_value");
+
+        editor.apply();
 
     }
+
 
 
 }
--- app/src/main/java/com/google/notes_2/fragment_1.java
+++ app/src/main/java/com/google/notes_2/fragment_1.java
@@ -2,14 +2,16 @@ package com.google.notes_2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class fragment_1 extends Fragment {
-
 
 
     @Override
@@ -18,10 +20,25 @@ public class fragment_1 extends Fragment {
 
     }
 
+    //Метод получение пользовательских элементов по идентификатору
+    public void initView(View view){
+        EditText etText = (EditText)view.findViewById(R.id.etText);
+        TextView tvNoteDate = (TextView)view.findViewById(R.id.tvNoteDate);;
+        EditText etTitle = (EditText)view.findViewById(R.id.etTitle);;
+
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+//        initView();
         return inflater.inflate(R.layout.fragment_1, container, false);
+
+
     }
+
+
+
 }
