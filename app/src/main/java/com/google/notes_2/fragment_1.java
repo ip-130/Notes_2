7f9309a58854a4599a44e07bda21c603cce1f55e
ip-130 <85837397+ip-130@users.noreply.github.com>
2021-07-08 16:38:55 +0300

    Добавил автоматическую установку даты и времени в заметке

--- app/src/main/java/com/google/notes_2/fragment_1.java
+++ app/src/main/java/com/google/notes_2/fragment_1.java
@@ -24,23 +24,15 @@ public class fragment_1 extends Fragment {
     TextView tvNoteDate;
     EditText etText;
     EditText etTitle;
-    Button timeButton;
-    Button dataButton;
     Calendar dateAndTime=Calendar.getInstance();
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setInitialDateTime();
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
         initView(view);
+        setInitialDateTime();
         return view;
     }
 
@@ -49,26 +41,8 @@ public class fragment_1 extends Fragment {
         etText = (EditText)view.findViewById(R.id.etText);
         tvNoteDate = (TextView)view.findViewById(R.id.tvNoteDate);
         etTitle = (EditText)view.findViewById(R.id.etTitle);
-        timeButton = (Button)view.findViewById(R.id.timeButton);
-        dataButton = (Button)view.findViewById(R.id.dateButton);
     }
 
-    // отображаем диалоговое окно для выбора даты
-    public void setDate(View v) {
-        new DatePickerDialog(getContext(),this.d,
-                dateAndTime.get(Calendar.YEAR),
-                dateAndTime.get(Calendar.MONTH),
-                dateAndTime.get(Calendar.DAY_OF_MONTH))
-                .show();
-    }
-
-    // отображаем диалоговое окно для выбора времени
-    public void setTime(View v) {
-        new TimePickerDialog(getContext(),this.t,
-                dateAndTime.get(Calendar.HOUR_OF_DAY),
-                dateAndTime.get(Calendar.MINUTE), true)
-                .show();
-    }
     // установка начальных даты и времени
     private void setInitialDateTime() {
         tvNoteDate.setText(DateUtils.formatDateTime(getContext(),
@@ -76,23 +50,4 @@ public class fragment_1 extends Fragment {
                 DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
                         | DateUtils.FORMAT_SHOW_TIME));
     }
-
-    // установка обработчика выбора времени
-    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            dateAndTime.set(Calendar.MINUTE, minute);
-            setInitialDateTime();
-        }
-    };
-
-    // установка обработчика выбора даты
-    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            dateAndTime.set(Calendar.YEAR, year);
-            dateAndTime.set(Calendar.MONTH, monthOfYear);
-            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            setInitialDateTime();
-        }
-    };
 }
